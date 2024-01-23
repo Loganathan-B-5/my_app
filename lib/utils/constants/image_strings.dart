b1cbf1b339cdbf5a06573414cc2cd6b73c0a01f4
Loganathan-B-5 <loganathanlogan2500@gmail.com>
2024-01-23 18:38:56 +0530

    Latest One

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,8 +2,8 @@ class TImages {
 
 
   //App Logos
-  static const String darkAppLogo = "assets/logos/logo_dark.svg";
-  static const String lightAppLogo = "assets/logos/logo_white.svg";
+  static const String darkAppLogo = "assets/logos/logo_dark.png";
+  static const String lightAppLogo = "assets/logos/logo_white.png";
 
   //Social Logos
   static const String google = "assets/logos/google.png";
